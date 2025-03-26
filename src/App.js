1bbc6b158e5f7a87753d365de1ad06d3d3ced40a
samuelpavanati <samuelnascimento278@gmail.com>
2025-03-26 09:16:39 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import HelloWorld from './components/HelloWorld'
+import SayMyName from './components/SayMyName'
+import Pessoa from './components/Pessoa'
+import List from './components/List'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HelloWorld />
+      <SayMyName name="Matheus" />
+      <SayMyName name="João" />
+      <SayMyName name="Francisco" />
+      <Pessoa
+        nome="Rodrigo"
+        idade="28"
+        profissao="Programador"
+        foto="https://via.placeholder.com/150"
+      />
+      <List />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/SayMyName.js
@@ -0,0 +1,9 @@
+function SayMyName(props) {
+  return (
+    <div>
+      <p>Fala aí {props.name}, suave?</p>
+    </div>
+  );
+}
+
+export default SayMyName;
